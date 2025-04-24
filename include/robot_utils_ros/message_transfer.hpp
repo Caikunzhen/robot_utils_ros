0e08cda266bb7f082a2b9265af5a8d056600d188
Caikunzhen <2210748751@qq,com>
2025-04-24 13:10:56 +0800

    (update)Add robot_utils to the current package

---
+++ include/robot_utils_ros/message_transfer.hpp
@@ -0,0 +1,26 @@
+/**
+ *******************************************************************************
+ * @file      : message_transfer.hpp
+ * @brief     :
+ * @history   :
+ *  Version     Date            Author          Note
+ *  V0.9.0      yyyy-mm-dd      <author>        1. <note>
+ *******************************************************************************
+ * @attention :
+ *******************************************************************************
+ *  Copyright (c) 2025 Caikunzhen, Zhejiang University.
+ *  All Rights Reserved.
+ *******************************************************************************
+ */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef ROBOT_UTILS_ROS_MESSAGE_TRANSFER_HPP_
+#define ROBOT_UTILS_ROS_MESSAGE_TRANSFER_HPP_
+
+/* Includes ------------------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported variables --------------------------------------------------------*/
+/* Exported function prototypes ----------------------------------------------*/
+
+#endif /* ROBOT_UTILS_ROS_MESSAGE_TRANSFER_HPP_ */
